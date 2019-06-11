73ae5b679f21b188c67061fdb834034ab0555706
Steven Williams <stevenwritescode@gmail.com>
2019-06-10 20:26:00 -0600

    Updates

---
+++ precache-manifest.404b842a01e2581bec5631ef43e2ffae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50948edfbeb13eba5319dd641ea23a53",
+    "url": "/retronator/index.html"
+  },
+  {
+    "revision": "9c4e2cfa72147f900c6f",
+    "url": "/retronator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f086a987c6f7c57b3a2d",
+    "url": "/retronator/static/js/2.dba58bda.chunk.js"
+  },
+  {
+    "revision": "9c4e2cfa72147f900c6f",
+    "url": "/retronator/static/js/main.9df64f81.chunk.js"
+  },
+  {
+    "revision": "6708479295c99b6a1bfe",
+    "url": "/retronator/static/js/runtime~main.73db179d.js"
+  }
+]);
